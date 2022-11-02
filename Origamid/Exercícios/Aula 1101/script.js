fd6d5461e9c11138504c2ad9e72964884d016fb5
Raphael Oliveira <raphavini@outlook.com>
2022-11-02 15:51:36 -0300

    começando javascript

---
+++ Origamid/Exercícios/Aula 1101/script.js
@@ -0,0 +1,12 @@
+const nome = 'Raphael';
+
+console.log(nome);
+
+/*const não pode mudar de valor, diferente de let*/
+
+let primeironome =  'JavaScript';
+primeironome = 'JS';
+
+console.log(primeironome);
+
+
