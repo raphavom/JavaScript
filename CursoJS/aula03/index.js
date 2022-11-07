11e8fd9f9b8ec4abf68a0b3a48a8efdf11f57206
Raphael Oliveira <raphavini@outlook.com>
2022-11-07 20:55:05 -0300

    java

---
+++ CursoJS/aula03/index.js
@@ -0,0 +1,5 @@
+console.log('Raphael "Oliveira"'); //Strings
+console.log("'Raphael' Oliveira");
+console.log(`'Raphael' "Oliveira"`);
+
+console.log(35, 15.85, "Raphael Oliveira")
---
+++ CursoJS/aula04/comentarios.js
@@ -0,0 +1,5 @@
+//escreve um comentário
+/*tem outro comentário
+esse comentário igual ao do html pode
+ter várias linhas
+*/
