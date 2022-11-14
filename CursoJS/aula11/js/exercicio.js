e39a9614233a8f98adf77656325371e2a9f6e45c
Raphael Oliveira <raphavini@outlook.com>
2022-11-14 15:35:11 -0300

    prompt e alert

---
+++ CursoJS/aula11/js/exercicio.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite o primeiro numero:');
+let num2 = prompt('Digite o segundo numero:');
+
+num1 = Number(num1)
+num2 = parseFloat(num2) //pode ser um dos dois
+
+let resultado = (num1 + num2)
+
+alert(`O resultado é ${resultado}`)
--- CursoJS/aula11/js/script.js
+++ CursoJS/aula11/js/script.js
@@ -1,3 +1,9 @@
-alert('Com a nossa mensagem') //atalho para window.alert()
-window.confirm('Deseja realmente apagar?')
-window.prompt('Digite seu nome:')
+// alert('Com a nossa mensagem') //atalho para window.alert()
+// window.confirm('Deseja realmente apagar?')
+// window.prompt('Digite seu nome:')
+//escritos diretamente no console do navegador
+// let confirma = confirm("Qualquer mensagem")
+let num1 = prompt("Digute um número")
+let num2 = prompt("Digute um número")
+num1 = parseFloat(num1)
+num1 = parseFloat(num2)
